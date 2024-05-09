005a7cf66ea1ed678f5fc7d9f38ee6b757ec1159
Deepanshu Gupta <97518034+de3de3@users.noreply.github.com>
2024-05-09 09:04:16 +0530

    Update 29.js

--- part1/29.js
+++ part1/29.js
@@ -6,6 +6,15 @@ let array1 = ["item1", "item2"];
 // let array2 = ["item1", "item2"];
 // let array2 = array1.slice(0).concat(["item3", "item4"]);
 // let array2 = [].concat(array1,["item3", "item4"]);
+
+
+// above, concat takes two arrays one empty another full and cerates a new array that has values of both the arrays
+
+
+
+The spread operator in JavaScript, denoted by three dots (...), is a versatile tool for expanding iterables (like arrays, objects, and strings) into individual elements in various contexts. Here's a breakdown of its uses:
+
+ekek element nuikal ke wityh commas separte kar deta hain
 // new way 
 // spread operator
 let oneMoreArray = ["item3", "item4"]
