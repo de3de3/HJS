17611a0424cb0adb3501dd0fbaed995214b9ff1f
Deepanshu Gupta <97518034+de3de3@users.noreply.github.com>
2024-05-10 08:55:24 +0530

    Update 31.js

--- part1/31.js
+++ part1/31.js
@@ -5,3 +5,8 @@
 // const fruits = ["apple", "mango"]; // 0x11
 // fruits.push("banana");
 // console.log(fruits);
+
+
+
+// while makng an array or a rfgerecnec dta type, alawys use const, since it cant be reassdigned by someone later, 
+// bnest practice
